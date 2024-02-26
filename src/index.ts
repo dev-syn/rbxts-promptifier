087459fdca23f2d1e4aa15db527f2aacafa36706
Casey <103451081+dev-syn@users.noreply.github.com>
2024-02-25 19:50:51 -0500

    Rework and Fix Prompt.Message

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { Prompt_Choice } from './types/Prompt_Choice';
 import { Prompt_Compact } from './types/Prompt_Compact';
 import { UIResolver } from './UIResolver';
 import { Timer, TimerPosition, TimerType } from './Timer';
-
-const MODULE_PREFIX: "[Promptifier]:" = "[Promptifier]:";
+import { t } from '@rbxts/t';
 
 /**
  * @category Prompt
@@ -56,6 +55,8 @@ const promptCompact: Prompt_Compact = script.FindFirstChild("PromptInstances")!.
 
 const player: Player = PlayersService.LocalPlayer;
 
+// #region MODULE_FUNCTIONS
+
 /** Creates the default UIListLayout inserted into Prompt._UI.content if it's a Scrolling Frame. */
 function createDefaultUIListLayout(): UIListLayout {
     const listLayout: UIListLayout = new Instance("UIListLayout");
@@ -70,8 +71,9 @@ function createDefaultUIListLayout(): UIListLayout {
 }
 
 /** Creates the default message label used for the Prompt.Message property. */
-function createDefaultMessageLabel(text: string): TextLabel {
+function createDefaultMessageLabel(text: string,bgColor?: Color3): TextLabel {
     const messageLabel: TextLabel = new Instance("TextLabel");
+    messageLabel.BackgroundColor3 = bgColor || Color3.fromRGB(60,60,60);
     messageLabel.Name = "Message";
     messageLabel.Size = new UDim2(1,0,0.2,0);
     messageLabel.Text = text;
@@ -113,7 +115,7 @@ function extractDataFromContent(content: ScrollingFrame | Frame,contentPayload: 
 
 function getPromptsScreenGui(): ScreenGui {
     const playerGui: PlayerGui | undefined = player.FindFirstChildWhichIsA("PlayerGui");
-    if (!playerGui) error(`${MODULE_PREFIX} No PlayerGui can be found.`);
+    if (!playerGui) error(`{Promptifier}: No PlayerGui can be found.`);
 
     let _promptsScreenUI: ScreenGui | undefined = playerGui.FindFirstChild("Prompts") as ScreenGui | undefined;
     if (!_promptsScreenUI) {
@@ -126,6 +128,8 @@ function getPromptsScreenGui(): ScreenGui {
     }
     return _promptsScreenUI;
 }
+
+// #endregion
 
 /**
  * @enum {number}
@@ -155,32 +159,59 @@ export interface PromptOptions {
 /**
  * @category Prompt
  * The main class used to create & use Prompts.
+ * @example
+ * const prompt: Prompt = new Prompt(PromptType.Choice,"ExamplePrompt","Example message") || new Prompt(PromptType.Compact,"ExamplePrompt","Example message") || new Prompt(PromptType.Custom,"ExamplePrompt","Example message");
+ * prompt.OnFulfill.Connect((accepted: boolean,payload: PromptPayload) => {
+ *     if (accepted && payload) {
+ *         if (payload.PromptContent.has("SomeInstanceName")) // Do something with data
+ *     }
+ *     prompt.Destroy();
+ * });
+ * 
+ * prompt.OnCancel.Connect((reason?: string) => {
+ *     warn("Prompt was cancelled for reason: " + reason);
+ *     // Other logic upon cancel
+ *     prompt.Destroy();
+ * });
+ * 
+ *
+ * // Tips:
+ * 
+ * // - You don't have to destroy the prompt, after it's first prompt it can be reused.
+ * 
+ * // - A prompt message that is passed into the contructor is used to display a text label automatically for you with text inside of it
+ * //   TODO: It will later be accessible through a function that will allow you to change some properties.
+ *
  */
 class Prompt<T extends TimerType | null = null> {
     static ClassName: string = "Prompt";
-    static _promptsScreenUI?: ScreenGui;
+
+// #region private_static_properties
+    private static _promptsScreenUI?: ScreenGui;
+// #endregion
 
     /** 
      * @public
      * The title of the Prompt.
      */
-    title: string;
+    Title: string;
     /**
      * @public
      * The message of the Prompt. This is optional and when toggled a TextLabel
      * will be added to content for you with your specified message.
      */
-    message?: string;
+    Message?: string;
     /**
      * @defaultValue `0` No timeout will be present, and the prompt will function indefinitely until Destroyed.
      * The timeout of this Prompt, when a prompt times out it will fullfill as declined.
      */
-    timeOut: number = 0;
+    TimeOut: number = 0;
 
     /** The configurable options of this Prompt. See {@link PromptOptions}*/
-    options: PromptOptions = {
+    Options: PromptOptions = {
         destroyOnTimeout: true,
-        timeoutBehavior: TimeoutBehavior.RejectOnTimeout
+        timeoutBehavior: TimeoutBehavior.RejectOnTimeout,
+
     };
 
     /**
@@ -265,30 +296,30 @@ class Prompt<T extends TimerType | null = null> {
     constructor(promptType: PromptType.Choice,title: string,message: string | undefined);
     constructor(promptType: PromptType,title: string,message: string | undefined,UI?: UIResolver) {
 
-        this.title = title;
-        this.message = message;
+        this.Title = title;
+        this.Message = message;
 
         if (promptType === PromptType.Custom) {
 
             // If the prompt is custom validate it
-            if (!UI) error("UIResolver must be given when using custom prompt type.");
+            if (!UI) this.__error("UIResolver must be given when using custom prompt type.");
 
-            UI.validate();
+            UI.Validate();
             this._UI = UI;
         } else if (promptType === PromptType.Compact) {
 
             const _promptCompact: Prompt_Compact = promptCompact.Clone();
 
             const resolver: UIResolver = new UIResolver();
-            const isResolvable: boolean = resolver.resolve({
-                BG: _promptCompact,
+            const isResolvable: boolean = resolver.Resolve({
+                bg: _promptCompact,
                 title: _promptCompact.Title,
                 content: _promptCompact.Content,
                 acceptBtn: _promptCompact.ConfirmBtn,
                 declineBtn: _promptCompact.CloseBtn
             });
 
-            if (!isResolvable) error("Failed to resolve UI Structure for prompt 'Compact' type.");
+            if (!isResolvable) this.__error("Failed to resolve UI Structure for prompt 'Compact' type.");
             this._UI = resolver;
 
         } else if (promptType === PromptType.Choice) {
@@ -296,38 +327,29 @@ class Prompt<T extends TimerType | null = null> {
             const _promptChoice: Prompt_Choice = promptChoice.Clone();
 
             const resolver: UIResolver = new UIResolver();
-            const isResolvable: boolean = resolver.resolve({
-                BG: _promptChoice,
+            const isResolvable: boolean = resolver.Resolve({
+                bg: _promptChoice,
                 title: _promptChoice.Title,
                 content: _promptChoice.Content,
                 acceptBtn: _promptChoice.YBtn,
                 declineBtn: _promptChoice.NBtn
             });
 
-            if (!isResolvable) error("Failed to resolve UI Structure for prompt 'Choice' type.");
+            if (!isResolvable) this.__error("Failed to resolve UI Structure for prompt 'Choice' type.");
             this._UI = resolver;
-        } else error(`Unknown PromptType: '${promptType}'`);
+        } else this.__error(`Unknown PromptType: '${promptType}'`);
 
         this._type = promptType;
-        this._UI.title.Text = this.title;
+        this._UI.Title.Text = this.Title;
+        
 
-        if (this._UI.content.IsA("ScrollingFrame")) {
+        if (this._UI.Content.IsA("ScrollingFrame")) {
 
             // Check for a UIListLayout adding one if it doesn't exist
-            let listLayout: UIListLayout | undefined = this._UI.content.FindFirstChildOfClass("UIListLayout");
-            if (!listLayout) {
-                listLayout = createDefaultUIListLayout();
-                listLayout.Parent = this._UI.content;
+            if (!this._UI.Content.FindFirstChildOfClass("UIListLayout")) {
+                const listLayout: UIListLayout = createDefaultUIListLayout();
+                listLayout.Parent = this._UI.Content;
             }
-        }
-
-        // Check if a message is present for this prompt
-        if (this.message) {
-
-            // Add a text label to the content to represent the message
-            const messageLabel: TextLabel = createDefaultMessageLabel(this.message);
-            messageLabel.LayoutOrder = getLowestLayoutOrder(this._UI.content) - 1;
-            messageLabel.Parent = this._UI.content;
 
         }
 
@@ -345,30 +367,56 @@ class Prompt<T extends TimerType | null = null> {
         // Only trigger if not already triggered
         if (this._triggered) return;
 
+        this._cancelled = false;
         this._triggered = true;
 
         // Adjust the ZIndex for this Prompt
         this._UI.BG.ZIndex = Prompt._promptsScreenUI!.GetChildren().size() + 1;
-        this._UI.assignZIndex();
+        this._UI.ReassignZIndex();
 
-        const incrementedZIndex: number = this._UI.BG.ZIndex + 1;
-        this._UI.content.ZIndex = incrementedZIndex;
+        this._UI.Title.Text = this.Title;
+
+// #region PROMPT_MESSAGE
+        // Check if a message is present for this prompt
+        if (this.Message) {
+
+            let msgInstance: Instance | undefined = this._UI.Content.FindFirstChild("Message");
+
+            // If an instance is found but it's not a text label then rename that instance
+            if (msgInstance && !t.instanceIsA("TextLabel")(msgInstance)) {
+                this.__warn(`Prompt '${this.Title}' An Instance named 'Message' is in the Prompt UI and it is a reserved child instance. It will be renamed to Message1.`);
+                msgInstance.Name = "Message1";
+                msgInstance = undefined;
+            }
+
+            let msgLabel: TextLabel | undefined = undefined;
+            if (!msgInstance) {
+                // Add a text label to the content to represent the message
+                msgLabel = createDefaultMessageLabel(this.Message,this._UI.BG.BackgroundColor3) as TextLabel;
+                msgLabel.LayoutOrder = getLowestLayoutOrder(this._UI.Content) - 1;
+                msgLabel.Parent = this._UI.Content;
+            } else msgLabel = msgInstance as TextLabel;
+
+            msgLabel.Text = this.Message;
+        }
+// #endregion
 
         // For each GuiObject in content; assign ZIndex + 1
-        (this._UI.content.GetChildren().filter(child => child.IsA("GuiObject")) as GuiObject[]).forEach(child => child.ZIndex = incrementedZIndex + 1);
+        const contextZIndex: number = this._UI.Content.ZIndex + 1;
+        (this._UI.Content.GetChildren().filter(child => child.IsA("GuiObject")) as GuiObject[]).forEach(child => child.ZIndex = contextZIndex);
 
+// #region UI_CONNECTION
         this._UIConnections.push(
-            this._UI.acceptBtn.MouseButton1Click.Connect(() => {
-                print(`${MODULE_PREFIX} Prompt has been accepted!`);
+            this._UI.AcceptBtn.MouseButton1Click.Connect(() => {
 
-                // Clean the UI connections connections
+                // Clean the UI connections connections since we reestablish every trigger
                 this.cleanConnections();
 
                 const promptPayload: PromptPayload<T> = {
                     prompt: this,
                     promptContent: new Map()
                 };
-                extractDataFromContent(this._UI.content,promptPayload.promptContent);
+                extractDataFromContent(this._UI.Content,promptPayload.promptContent);
 
                 // If the Prompt is not validated or cancelled during validation then return.
                 if (this.Validator) {
@@ -389,9 +437,8 @@ class Prompt<T extends TimerType | null = null> {
 
         // Listen for when the prompt is declined
         this._UIConnections.push(
-            this._UI.declineBtn.MouseButton1Click.Connect(() => {
-                print(`${MODULE_PREFIX} Prompt has been declined!`);
-                // Clean the UI connections connections
+            this._UI.DeclineBtn.MouseButton1Click.Connect(() => {
+                // Clean the UI connections connections since we reestablish every trigger
                 this.cleanConnections();
 
                 this._UI.BG.Visible = false;
@@ -399,51 +446,34 @@ class Prompt<T extends TimerType | null = null> {
                 this._triggered = false;
             })
         );
+
+// #endregion
+
 // #region TIMEOUT_HANDLER
 
-        if (this.timeOut > 1) {
+        if (this.TimeOut > 1) {
 
             // Create a timer for this prompt
             if (!this._timer) {
+                // If no timer is assigned but a time out is present then create the defaults
                 if (this._type === PromptType.Choice) {
-                    this._timer = new Timer<TimerType.Bar>(TimerType.Bar,this.timeOut) as T extends TimerType ? Timer<T> : undefined;
-                    this._timer!.parentBorderSize = this._UI.BG.BorderSizePixel;
+                    this._timer = new Timer<TimerType.Bar>(TimerType.Bar,this.TimeOut) as T extends TimerType ? Timer<T> : undefined;
+                    this._timer!.ParentBorderSize = this._UI.BG.BorderSizePixel;
                     this._timer!.SetPosition(TimerPosition.Bottom);
                 } else if (this._type === PromptType.Compact) {
-                    this._timer = new Timer<TimerType.Digit>(TimerType.Digit,this.timeOut) as T extends TimerType ? Timer<T> : undefined;
-                    this._timer!.parentBorderSize = this._UI.BG.BorderSizePixel;
+                    this._timer = new Timer<TimerType.Digit>(TimerType.Digit,this.TimeOut) as T extends TimerType ? Timer<T> : undefined;
+                    this._timer!.ParentBorderSize = this._UI.BG.BorderSizePixel;
                     this._timer!.SetPosition(TimerPosition.BottomLeft);
-                }
+                } else this.__warn(`Prompt '${this.Title}' is a PromptType.Custom type and will not be timed out since no timer was assigned.`);
             }
 
             // If a timer is present; start the timer
             if (this._timer) {
-                this._timer.SetZIndex(incrementedZIndex);
+                this._timer.SetZIndex(this._UI.BG.ZIndex);
                 this._timer._timeUI.Parent = this._UI.BG;
-                this._timer.Set(this.timeOut);
+                this._timer.Set(this.TimeOut);
 
-                task.defer((prompt: Prompt<T>) => {
-                    let initial: number = os.time();
-                    while (prompt._triggered && !prompt._cancelled && !prompt._destroyed) {
-    
-                        if (prompt._timer) prompt._timer.Decrement();
-    
-                        // Check if the prompt has timed out
-                        if (os.difftime(os.time(),initial) >= prompt.timeOut) {
-                            prompt._UI.BG.Visible = false;
-
-                            if (prompt.options.timeoutBehavior === TimeoutBehavior.CancelOnTimeout)
-                                prompt.OnCancel.Fire("Prompt has timed out.");
-                            else prompt.OnFulfill.Fire(false);
-    
-                            if (prompt.options.destroyOnTimeout) {
-                                // Destroy the Prompt since it has timed out.
-                                prompt.Destroy();
-                            } else break; // Remember to break to ensure that this thread closes.
-                        }
-                        task.wait(1);
-                    }
-                },this);
+                task.defer(this.startPromptTimer);
             }
         }
 
@@ -478,7 +508,10 @@ class Prompt<T extends TimerType | null = null> {
             this._timer.Destroy();
             this._timer = undefined;
         }
-        this._UI.BG.Destroy();
+        if (this._UI.BG) {
+            this._UI.BG.Destroy();
+            this._UI.BG = undefined!;
+        }
         this._UI = undefined!;
         this._destroyed = true;
     }
@@ -489,7 +522,43 @@ class Prompt<T extends TimerType | null = null> {
      */
     private cleanConnections() {
         this._UIConnections.forEach(conn => conn.Disconnect());
-        this._UIConnections = [];
+        this._UIConnections.clear();
+    }
+
+    /**
+     * @private
+     * This starts and handles the prompts internal timing and functionality when {@link Prompt.TimeOut} is assigned.
+     */
+    private startPromptTimer() {
+        let initial: number = os.time();
+        while (!this._destroyed && this._triggered && !this._cancelled) {
+            if (this._timer) this._timer.Decrement();
+
+            // Check if the prompt has timed out
+            if (os.difftime(os.time(),initial) >= this.TimeOut) {
+                this._UI.BG.Visible = false;
+
+                if (this.Options.timeoutBehavior === TimeoutBehavior.CancelOnTimeout)
+                    this.OnCancel.Fire("Prompt has timed out.");
+                else this.OnFulfill.Fire(false);
+
+                if (this.Options.destroyOnTimeout) {
+                    // Destroy the Prompt since it has timed out.
+                    this.Destroy();
+                }
+
+                break; // Once timed out no reason to stay here
+            }
+
+            task.wait(1);
+        }
+    }
+
+    private __warn(...params: unknown[]) { warn(`{Promptifier}: `,...params); }
+    private __error(message?: unknown,level?: number): never {
+        if (!level) level = 1;
+        if (typeof message === "string") error(`{Promptifier}: ${message}`,level + 1);
+        else error(message,level);
     }
 
 };
